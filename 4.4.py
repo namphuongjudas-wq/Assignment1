b89970a338aaccce4ec885b8362b7e9a5425a0f8
namphuongjudas-wq <namphuongjudas@gmail.com>
2026-03-02 19:41:41 +0700

    Refactor phone number input to a function

--- 4.4.py
+++ 4.4.py
@@ -1,14 +1,16 @@
-phone_number_list = []
+def phone_number_def():
 
-while True:
-    phone_number = input("Type your phone number here: ")
-    
-    if phone_number.startswith("0") or phone_number.startswith("+84"):
-        phone_number_list.append(phone_number)
-        anonymous = "#" * (len(phone_number)-3) + phone_number[7:]
-        print(f"Your phone number is anonymized:{anonymous}")
-        break
-    
-    else:
-        print("try again")
+    while True:
+        phone_number = input("Type your phone number here: ")
+        
+        if phone_number.startswith("0") or phone_number.startswith("+84"):
+            anonymous = "#" * (len(phone_number)-3) + phone_number[7:]
+            return anonymous
+
+        
+        else:
+            print("try again")
+
+print(f"Your phone number is anonymized: {phone_number_def()}")
+
 
